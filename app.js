a42c6b975dbd7c5e8973ad98f6cf5f492d823a5c
EricM76 <menaericdaniel@gmail.com>
2022-06-27 20:03:59 -0300

    add routes

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
+app.get('/about', (req,res) => res.sendFile(path.join(__dirname, 'views', 'about.html')));
+app.get('/music', (req,res) => res.sendFile(path.join(__dirname, 'views', 'music.html')));
+app.get('/contact', (req,res) => res.sendFile(path.join(__dirname, 'views', 'contact.html')));
 
 
 app.listen(port, () => console.log('Servidor corriendo en http://localhost:' + port))
